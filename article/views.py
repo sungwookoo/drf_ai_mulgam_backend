c15de786fd336588b5f9e9b7f089dc6459437715
gyuhyeongH <ckacl792@gmail.com>
2022-06-28 18:07:18 +0900

    feat : article, category 모델 작성. article post기능 작성 url연결

--- article/views.py
+++ article/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+import os
 
-# Create your views here.
+
+class ArticleView(APIView):
+
+    def get(self, request):
+        return Response(status=status.HTTP_200_OK)
+
+    def post(self, request):
+        a = 'style_transfer data/12.jpg data/content.jpg'
+        os.system(a)
+        return Response({"message":"성공"}, status=status.HTTP_200_OK)
