4ec05457c926143251877ef8d29a737f92458571
gyuhyeongH <ckacl792@gmail.com>
2022-06-30 17:31:59 +0900

    feat : articleserializer custom create update 추가

--- article/serializers.py
+++ article/serializers.py
@@ -11,11 +11,26 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class ArticleSerializer(serializers.ModelSerializer):
     category = serializers.SerializerMethodField()
+    user = serializers.SerializerMethodField()
 
+    def get_user(self, obj):
+        return obj.user.username
 
     def get_category(self, obj):
         return [category.name for category in obj.category.all()]
 
+    def create(self, validated_data):
+
+        article = Article(**validated_data)
+        article.save()
+        return article
+
+    def update(self, instance, validated_data):
+        # instance에는 입력된 object가 담긴다.
+        instance.title = validated_data.title
+        instance.save()
+        return instance
+
     class Meta:
         model = Article
-        fields = ["user","title","category","img_url"]
+        fields = ["user", "title", "category", "img_url"]
