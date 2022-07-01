b9f2a81484b7ec911661cdab1c22df3002af36af
sungwookoo <sungwookoo.dev@gmail.com>
2022-07-01 19:10:30 +0900

    chore : allowed_hosts 변경

--- mulgam/settings.py
+++ mulgam/settings.py
@@ -32,9 +32,9 @@ def get_secret(setting, secrets=secrets):
     try:
         return secrets[setting]
     except KeyError:
-        return ""
-        # error_msg = "Set the {} environment variable".format(setting)
-        # raise ImproperlyConfigured(error_msg)
+        # return ""
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
 
 
 SECRET_KEY = get_secret("SECRET_KEY")
@@ -43,7 +43,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 DEBUG = True
 
 # EC2 서버 api 테스트를 위해 등록
-ALLOWED_HOSTS = [get_secret("ALLOWED_HOSTS")]
+ALLOWED_HOSTS = get_secret("ALLOWED_HOSTS")
 # ALLOWED_HOSTS = []
 
 # Application definition
